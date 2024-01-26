eb60529b98f99f26e1a4428538b7d92b54f6dde7
Sonualam <sonualam1599@gmail.com>
2024-01-26 18:20:46 +0530

    project starter template ready

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-[#fffc00] ">
+      <div className="min-h-screen flex flex-col items-center justify-center max-w-7xl mx-auto ">
+        THis is home page
+      </div>
+    </main>
+  );
+}
