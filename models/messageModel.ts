b01d8f27d8c41802d22cc2745ad76ee4846641d2
Sonualam <sonualam1599@gmail.com>
2024-01-30 00:12:33 +0530

    implemented chatModel and messageModel

---
+++ models/messageModel.ts
@@ -0,0 +1,43 @@
+import mongoose, { Document, Model, Schema, Types } from "mongoose";
+
+export interface IMessage{
+    sender: Types.ObjectId;
+    receiver: Types.ObjectId;
+    content: string,
+    messageType: "text" | "image";
+    opened: boolean
+
+}
+
+export interface IMessageDocument extends IMessage, Document {
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const messageSchema = new Schema<IMessageDocument>({
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    receiver: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    messageType: {
+        type: String,
+        required: true,
+        enum: ["text", "image"]
+    },
+    opened: {
+        type: Boolean,
+        default: false
+    }
+})
+
+export const Message: Model<IMessageDocument> = mongoose.models?.Message || mongoose.model("Message", messageSchema)
